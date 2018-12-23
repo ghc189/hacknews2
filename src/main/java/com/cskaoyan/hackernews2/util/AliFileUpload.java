771f2929e5449bcebf67aebe2cda650b1deeebe5
ghc189 <2993223351@qq.com>
2018-12-23 17:01:55 +0800

    Update AliFileUpload.java

--- src/main/java/com/cskaoyan/hackernews2/util/AliFileUpload.java
+++ src/main/java/com/cskaoyan/hackernews2/util/AliFileUpload.java
@@ -16,8 +16,8 @@ public class AliFileUpload {
     // endpoint以杭州为例，其它region请按实际情况填写
     String endpoint = "http://oss-cn-shenzhen.aliyuncs.com";
     // 云账号AccessKey有所有API访问权限，建议遵循阿里云安全最佳实践，创建并使用RAM子账号进行API访问或日常运维，请登录 https://ram.console.aliyun.com 创建
-    String accessKeyId = "LTAIFlFQHLKIUgg7";
-    String accessKeySecret = "5QnfPOFWHW9evIVXjhDbLqtHSebpWu";
+    String accessKeyId = "*********";
+    String accessKeySecret = "************";
 
 
     String buketname=    "cskaoyanghc";
